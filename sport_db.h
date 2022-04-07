807a4d08e03c398a114a9bdd101caa38c668b612
samiromero <69342885+samiromero@users.noreply.github.com>
2022-04-07 15:01:28 -0400

    Create sport_db.h

---
+++ sport_db.h
@@ -0,0 +1,12 @@
+//Header containing macros and definitions for sport_db.c
+
+#include "sport_events.h"
+#include "sport_athletes.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//void print_help(void);
+
+int main(void);
